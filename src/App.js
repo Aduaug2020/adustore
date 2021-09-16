6b7652a2249194fab25a148acdf09bc0a3da06a6
Adugna Beyene <beyeneadu1@gmail.com>
2021-09-15 22:19:50 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./css/App.css";
+import Header from "./Components/Header";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from "./Components/Checkout";
+import React from "react";
+import Product from "./Components/Product";
+import { loadStripe } from "@stripe/stripe-js";
+import DetailPage from "./Components/DetailPage";
+import CheckoutPage from "./Components/CheckoutPage";
+import { Elements } from "@stripe/react-stripe-js";
+const promise = loadStripe("pk_test_51Hf9EsIjq2KMm1vIrtyaI9OtGTvf45UJtZJgN3TPTmOaTQ5IAqHwTs4YIhZCQNNphj6NkIAFZN75pLloPCwMrsZz00SVhcXIq6");
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          
+          <Route path="/cart"><Header /><Checkout /></Route>
+          <Route path="/DetailPage"><Header /><DetailPage /></Route>
+          <Route path="/checkoutpage"><Elements stripe={promise}><Header /><CheckoutPage /></Elements></Route>
+          <Route path="/"><Header /><Product /></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
