947e8ca5f296ce92199646efc2423efedd00ce51
Adugna Beyene <beyeneadu1@gmail.com>
2021-09-16 13:16:06 -0700

    update commit

--- src/Components/CheckoutProduct.js
+++ src/Components/CheckoutProduct.js
@@ -12,69 +12,6 @@ function CheckoutProducts({ id, image, title, price }) {
     });
   };
 
-
-//   // for adding item to cart
-// const AddToCart =(id)=>dispatch=>{
-
-//  dispatch({
-//      type:ADD_ITEMS,
-//      payload:id
-//  });
-// };
-
-// //    for deleting the items from the cart
-
-//  const deleteItem =(id)=>dispatch=>{
-
-//     dispatch({
-//         type:DELETE_ITEM,
-//         payload:id
-    
-//     })
-//     }
-
-
-//    //    for increasing item inside cart
-
-//  const increaseItem =(id)=>dispatch=>{
-
-//     console.log(`increase :${id}`);
-//     dispatch({
-//         type:INCREASE_ITEM,
-//         payload:id
-    
-//     })
-   
-//    }
-
-
-//       //    for increasing item inside cart
-
-// export const decreaseItem =(id)=>dispatch=>{
-
-//     console.log(`DECREASE :${id}`);
-//     dispatch({
-//         type:DECREASE_ITEM,
-//         payload:id
-    
-//     })
-   
-//    }
-
-//          //  for getting the total price of items inside the cart
-
-// export const getTotals =()=>dispatch=>{
-
-//     dispatch({
-//         type:TOTAL_ITEMS,
-      
-//     })
-   
-//    }
-
-
-
-
   return (
     <div className="checkoutProduct">
       <img className="checkoutProduct__image" src={image} alt=""/>
